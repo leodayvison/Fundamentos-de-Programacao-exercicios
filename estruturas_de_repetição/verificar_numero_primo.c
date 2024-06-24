1da26c4935503bf552993e24a6ba02ba361cba40
leodayvison <leonardodayvison235@gmail.com>
2024-06-23 22:30:44 -0300

    questão da primaridade resolvida

--- estruturas_de_repetição/verificar_numero_primo.c
+++ estruturas_de_repetição/verificar_numero_primo.c
@@ -5,10 +5,10 @@ int main(){
     int numero;
     bool resposta = true; //a resposta inicia com verdadeiro, pois os loops vão checar se existe um caso que falsifica a tese de que o numero é primo
     printf("digite um numero: ");
-    scanf("&d", &numero);
+    scanf("%d", &numero);
     
     //primeira solução (SIMULANDO FOR COM WHILE):
-    int i = numero; //variável de comando
+    int i = numero - 1; //variável de comando
     while(i>=2){
         if(numero%i == 0){
             resposta = false; //transforma a resposta em false caso encontre um divisor
@@ -16,7 +16,7 @@ int main(){
         i--;
     }
     //segunda solução (SIMULANDO FOR COM WHILE TRUE):
-    int j = numero; //variável de comando
+    int j = numero - 1; //variável de comando
     while(true){
         if(numero%j == 0){
             resposta = false;
@@ -29,13 +29,18 @@ int main(){
     }
 
     //terceira solução (EXERCITANDO A CONDIÇÃO WHILE):
-    int k = numero; //variável de comando
+    int k = numero - 1; //variável de comando
     while (numero%k != 0){ //verifica a primaridade, e para se o numero não for primo
     k--;
     }
     if(k>=2){
         resposta = false; //o loop vai terminar quando a variável de comando for 1, pois todos os numeros sao divisiveis por ele, entao o numero só não será primo se a variável tiver terminado sendo maior que 1
     }
-    printf("%d", resposta);
+    if(resposta == true){
+        printf("%d", 1);
+    }
+    else{
+        printf("%d", 0);
+    }
     return 0;
 }
