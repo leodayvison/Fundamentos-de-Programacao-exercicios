abd50aafc946d3e3e373a13382e3cbd45ad2667b
leodayvison <leonardodayvison235@gmail.com>
2024-08-31 10:15:41 -0300

    DEU TUDO CERTO

--- seilanemestudomaispraessacadeira/questaoaiqueojoaopaulofez/resolucao.c
+++ seilanemestudomaispraessacadeira/questaoaiqueojoaopaulofez/resolucao.c
@@ -1,36 +1,52 @@
 #include <stdio.h>
-#include <math.h>
 
-int main(){
-    int sinal=0, indice=5, numerador=1, divisor=1, fatores[15], result, x, fatorAtual;
+int main() {
+    int sinal = 0, indice = 1;
+    double fatorAtual, x, numerador, divisor, result = 0, fatores[15];
+    
+    // Lendo o valor de x
     printf("Digite o valor de X, em graus: ");
-    scanf("%d", &x);
-    for(int i=0; i<15; i++){
-        //fazendo numerador
-        for(int j=0; j<indice; j++){
+    scanf("%lf", &x);
+    
+    // Convertendo de graus para radianos
+    x = x * (3.1415926 / 180.0);
+    
+    for(int i = 0; i < 15; i++) {
+        numerador = divisor = 1;
+        
+        // Calculando o divisor (fatorial)
+        for(int j = indice; j > 0; j--) {
+            divisor *= j;
+        }
+
+        // Calculando o numerador (potência de x)
+        for(int j = 0; j < indice; j++) {
             numerador *= x;
         }
 
-        //fazendo divisor
-        for(int j=indice; j>1; j--){
-            divisor *= i;
-        }
-        //calculando e armazenando fator atual
-        fatorAtual = numerador/divisor;
+        // Calculando e armazenando o fator atual
+        fatorAtual = numerador / divisor;
         fatores[i] = fatorAtual;
 
-        if(sinal==0){
-            result +=fatorAtual;
+        // Atualizando o resultado
+        if(sinal == 0) {
+            result += fatorAtual;
             sinal++;
-        }
-        else{
+        } else {
             result -= fatorAtual;
             sinal--;
         }
-        indice +=2;
+        
+        indice += 2; // Atualiza o índice para a próxima iteração
     }
-    printf("O seno de X eh: %d\n", result);
-    for(int i=0; i<15; i++){
-        printf("%d, ", fatores[i]);
+
+    // Imprimindo o resultado
+    printf("O seno de X eh: %lf\n", result);
+    
+    // Imprimindo os fatores para verificar
+    for(int i = 0; i < 15; i++) {
+        printf("%f, ", fatores[i]);
     }
+
+    return 0;
 }
